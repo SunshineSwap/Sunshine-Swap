157787b914c0a29f55614c47e057c893fc522da4
aeither <36173828+aeither@users.noreply.github.com>
2021-03-02 01:52:47 +0100

    Add Torus 1

---
+++ src/helper.js
@@ -0,0 +1,24 @@
+import Web3 from 'web3'
+import Torus from '@toruslabs/torus-embed'
+
+const web3Obj = {
+  web3: new Web3(),
+  torus: {},
+  setweb3: function (provider) {
+    const web3Inst = new Web3(provider)
+    web3Obj.web3 = web3Inst
+  },
+  initialize: async function (buildEnv) {
+    const torus = new Torus()
+    await torus.init({   network: {
+      host: "https://ethboston1.skalenodes.com:10062", // mandatory
+      chainId: 1, // optional
+      networkName: "Skale Network" // optional
+    }|| 'production', network: { host: 'rinkeby' } })
+    await torus.login()
+    web3Obj.setweb3(torus.provider)
+    web3Obj.torus = torus
+    sessionStorage.setItem('pageUsingTorus', buildEnv)
+  }
+}
+export default web3Obj
